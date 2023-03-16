65ece1684b59e35ba9f185884ab8e0a366499b75
Reinier <rnuviola@gmail.com>
2023-03-16 09:41:27 -0400

    Backend: Added test to Post Model

--- backend/django_project/settings/base.py
+++ backend/django_project/settings/base.py
@@ -32,7 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #Local
-    'applications.accounts.apps.AccountsConfig'
+    'applications.accounts.apps.AccountsConfig',
+    'applications.posts',
 ]
 
 MIDDLEWARE = [
