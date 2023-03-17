7c8309002cef0d0b724c6f4c9cce1078adbc8d5e
Reinier <rnuviola@gmail.com>
2023-03-17 11:13:57 -0400

    Added signup and login components

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+    const [usernameEntered, setUsernameEntered] = useState("");
+    const [passwordEntered, setPasswordEntered] = useState("");
+    const usernameHandler = (event) => {
+        setUsernameEntered(event.target.value);
+    }
+    const passwordHandler = (event) => {
+        setPasswordEntered(event.target.value);
+    }
+    const submitHandler = (event) => {
+        event.preventDefault();
+        if (
+            passwordEntered.trim().length === 0 ||
+            usernameEntered.trim().length === 0
+        ) {
+            // props.onError(true);
+            return null;
+        }
+        const userData = {
+            username: usernameEntered,
+            password: passwordEntered,
+        };
+        //   props.onAddUserData(userData);
+        setUsernameEntered("");
+        setPasswordEntered("");
+
+    }
     return (
-        <h1>Login</h1>
+        <>
+            <h1>Login</h1>
+            <form onSubmit={submitHandler}>
+                <input onChange={usernameHandler}
+                    type="text"
+                    name="username"
+                    placeholder="Enter user name"
+                    value={usernameEntered}
+                ></input>
+                <input onChange={passwordHandler}
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    value={passwordEntered}
+                ></input>
+                <button type="submit">Login</button>
+            </form>
+            <p >
+                Don't have account? <Link to="/signup">Signup</Link>
+            </p>
+        </>
     );
 }
 
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,11 +1,60 @@
 import React from "react";
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
+
+    const [usernameEntered, setUsernameEntered] = useState("");
+
+    const [passwordEntered, setPasswordEntered] = useState("");
+
+    const usernameHandler = (event) => {
+        setUsernameEntered(event.target.value);
+    }
+    const passwordHandler = (event) => {
+        setPasswordEntered(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        if (
+            passwordEntered.trim().length === 0 ||
+            usernameEntered.trim().length === 0
+        ) {
+            // props.onError(true);
+            return null;
+        }
+        const userData = {
+            username: usernameEntered,
+            password: passwordEntered,
+        };
+        //   props.onAddUserData(userData);
+        setUsernameEntered("");
+        setPasswordEntered("");
+
+    }
     return (
-
-        <h1>Signup</h1>
-
+        <>
+            <h1>Signup</h1>
+            <form onSubmit={submitHandler}>
+                <input onChange={usernameHandler}
+                    type="text"
+                    name="username"
+                    placeholder="Enter user name"
+                    value={usernameEntered}
+                ></input>
+                <input onChange={passwordHandler}
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    value={passwordEntered}
+                ></input>
+                <button type="submit">Sign Up</button>
+            </form>
+            <p >
+              Already have account? <Link to="/login">Login</Link>
+            </p>
+        </>
     );
 }
 
